c5b8c4fdac4f6133930735677d250876185eb330
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-11-08 14:19:01 +0530

    ✨ feat: fetching data using React Routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,42 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Home from './ui/Home';
-import Menu from './features/menu/Menu';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import Order from './features/order/Order';
 import CreateOrder from './features/order/CreateOrder';
 import AppLayout from './ui/AppLayout';
 
-/** NESTING ROUTE
- * 1) import the AppLayout
- * 2) create a new object in createBrowserRouter, and use the imported AppLayout
- * 3) do not give any path, doing so will make it a layout route. A route with only
- * purpose of providing a layout to the application
- ** {
- **   element: <AppLayout/>
- ** }
- * 4) Now to make all of the routes below AppLayout's child route(nested route)
- * i) we define children property, and pass in a new array of routes
- ** {
- **   element: <AppLayout/>
- **   children: []
- ** }
- * ii) pass in all routes which needs to be nested inside the AppLayout
- * 5) Now we need to render the content of nested routes
- * i) So in Applayout we have, CartOverview contents and we see a
- * link Open cart ->, which will Cart component, so how do we now render the
- * Cart component Ui ?
- * ! figure 1, 1.1
- * in these figure you can see nothing happpens !
- * ii) So to bring output, we will use the <Outlet/> component in App layout:
- * ! figure 2
+/** ROUTERS LOADERS
+ * So the idea behind the loader is that, somewhere in our code
+ * we create a function that fetches us some data from Api.
+ * we then provide that loader function to one of our routes
+ * and then that route will fetch data as soon as the application
+ * goes to that route and then in the end once the data has arrived.
+ * It will be provided to the page component itselfs using a custom hook
  *
- * 6) So to sum up,
- * i) we created the <AppLayout/> component, which we want to use as a parent route
- * of our every single other other route that we have in our application, that's why
- * we placed <AppLayout/> at the top and then we made all the other routes, child routes
- * of the App layout, so they are all nested routes now.
- * ii) So then inside the the parent route which is <AppLayout/>, we used the <Outlet/>
- * component to render what ever is the current nested route.
+ * Let's implement and try this for a better understanding, it is done in 3 steps
+ * 1) We create a loader
+ * 2) We provide a loader
+ * 3) We provide the data to the page
+ *
+ * Now this data loader can be placed anywhere in our page,
+ * but the convention seems to be to place the loader for the data of a certain page
+ * inside the file of that page, So in our case as we need to load all the Menu data,
+ * So we will do this work in the Menu Component.
+ *
+ * 3) Once you are now back from Menu (continuation....)
+ * i) So we need to connect the loader function to the route,
+ * So in order to import the named export we do it like this and also rename it:
+ * * import Menu, {loader as menuLoader} from './features/menu/Menu';
+ * ii) now in createBrowserRouter, in menu object now we can specify the
+ * loader property and then specify the menuLoader as value:
+ * *  loader: menuLoader,
+ * give it a save and step no. 2 is complete.
+ * So now we have successfully provided loader function to the menu route
+ * iii) now all we have to do is get that data into the component
+ *
+ * 4)For that go back to Menu component (continue.. rest over there)
  */
 
 const router = createBrowserRouter([
@@ -51,6 +50,7 @@ const router = createBrowserRouter([
       {
         path: '/menu',
         element: <Menu />,
+        loader: menuLoader,
       },
       {
         path: '/cart',
