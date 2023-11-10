76aa6a030e7b8d9005f4b2c3870f63c1642f21dc
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-11-11 02:02:54 +0530

    ✅ feat: modelling the cartSlice

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/user/userSlice';
+import cartReducer from './features/cart/cartSlice';
 
 console.log(userReducer);
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cart: cartReducer,
   },
 });
 
